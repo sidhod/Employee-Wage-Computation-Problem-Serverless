108ef2f7eff13f8137f9dda6f73d144b2c4b825c
unknown <sidhodhank2018@gmail.com>
2022-12-15 13:13:12 +0530

    [Sidhodhan]Modify: Lambda Function For Calculate Employee Wage No Of Days

--- my-service/functions/calculateWageNoWorkingDays.js
+++ my-service/functions/calculateWageNoWorkingDays.js
@@ -1,16 +1,18 @@
 'use strict';
-module.exports.calculateWageNoWorkingDays = async () => {
+module.exports.calculateWageNoWorkingDays = async (event, contex, callback) => {
     var IS_Full_Time = 1;
     var IS_Part_Time = 2;
-    var EMP_RATE_PER_HOUR = 20;
-    var NO_OF_WORKING_DAYS = 20;
-    var MAX_HRS_IN_MONTH = 100;
+    //var EMP_RATE_PER_HOUR = 20;
+    //var NO_OF_WORKING_DAYS = 20;
+    //var MAX_HRS_IN_MONTH = 100;
+    let data = JSON.parse(event.body)
+    console.log(data);
     var empHrs = 0;
     var empWage = 0;
     var totalEmployeeWage = 0;
     var totalEmpHrs = 0;
     var totalWorkingDays = 0;
-    while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NO_OF_WORKING_DAYS) {
+    while (totalEmpHrs < data.MAX_HRS_IN_MONTH && totalWorkingDays < data.NO_OF_WORKING_DAYS) {
         totalWorkingDays++;
         var empCheck = Math.floor(Math.random() * 3) + 0;
         switch (empCheck) {
@@ -23,9 +25,12 @@ module.exports.calculateWageNoWorkingDays = async () => {
             default:
                 empHrs = 0;
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
+        empWage = empHrs * data.EMP_RATE_PER_HOUR;
         totalEmployeeWage = empWage + totalEmployeeWage;
     }
-    return `Total Employee Wage of Month:${totalEmployeeWage}`;
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Total Employee Wage of Month:${totalEmployeeWage}` })
+    })
 };
 
