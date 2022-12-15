561c934324fea726ab1de2c8d50f292c42a604f0
unknown <sidhodhank2018@gmail.com>
2022-12-15 12:40:28 +0530

    [Sidhodhan]Modify: Lambda Function For Calculate Employee Wage

--- my-service/functions/calculateEmployeeWage.js
+++ my-service/functions/calculateEmployeeWage.js
@@ -1,7 +1,8 @@
 'use strict';
-module.exports.calculateEmployeeWage = async () => {
-    var IS_Full_Time = 1;
-    var EMP_RATE_PER_HOUR = 20;
+module.exports.calculateEmployeeWage = async (event, contex, callback) => {
+    const IS_Full_Time = 1;
+    let data = JSON.parse(event.body);
+    console.log(data);
     var empHrs = 0;
     var empWage = 0;
     var empCheck = Math.floor(Math.random() * 2) + 0;
@@ -9,8 +10,11 @@ module.exports.calculateEmployeeWage = async () => {
         empHrs = 8;
     else
         empHrs = 0;
-    empWage = empHrs * EMP_RATE_PER_HOUR;
-    return `Emp Wage: ${empWage}`;
+    empWage = empHrs * data.EMP_RATE_PER_HOUR;
+    callback(null, {
+        statusCode: 200,
+        body: `Emp Wage: ${empWage}`
+    })
 
 };
 
