a2d33565e5869449551a2562fb9acf46bf75b87e
unknown <sidhodhank2018@gmail.com>
2022-12-15 13:00:39 +0530

    [Sidhodhan]Modify: Lambda Function For Calculate Employee Wage For Month

--- my-service/functions/calculateEmployeeWageForMonth.js
+++ my-service/functions/calculateEmployeeWageForMonth.js
@@ -1,13 +1,13 @@
 'use strict';
-module.exports.employeeWageForMonth = async () => {
-    var IS_Full_Time = 1;
-    var IS_Part_Time = 2;
-    var EMP_RATE_PER_HOUR = 20;
-    var NO_OF_WORKING_DAYS = 20;
+module.exports.employeeWageForMonth = async (event, contex, callback) => {
+    const IS_Full_Time = 1;
+    const IS_Part_Time = 2;
+    let data = JSON.parse(event.body)
+    console.log(data);
     var empHrs = 0;
     var empWage = 0;
     var totalEmployeeWage = 0;
-    for (var day = 0; day <= NO_OF_WORKING_DAYS; day++) {
+    for (var day = 0; day <= data.NO_OF_WORKING_DAYS; day++) {
         var empCheck = Math.floor(Math.random() * 3) + 0;
         switch (empCheck) {
             case IS_Part_Time:
@@ -19,9 +19,12 @@ module.exports.employeeWageForMonth = async () => {
             default:
                 empHrs = 0;
         }
-        empWage = empHrs * EMP_RATE_PER_HOUR;
+        empWage = empHrs * data.EMP_RATE_PER_HOUR;
         totalEmployeeWage = empWage + totalEmployeeWage;
     }
-    return `Total Employee Wage of Month:${totalEmployeeWage}`;
+    callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ message: `Total Employee Wage of Month:${totalEmployeeWage}` })
+    })
 };
 
